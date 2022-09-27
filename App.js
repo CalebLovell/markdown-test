f47d3112e8b065a4e1298cc6366e6ece42014f07
CalebLovell <caleblovell1@gmail.com>
2022-09-27 21:04:54 +0200

    create minimal reproducible demo

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import Markdown from './components/Markdown';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<QueryClientProvider client={queryClientConfig}>
+			<View style={styles.container}>
+				<Text>Markdown Display with EAS</Text>
+				<StatusBar style='auto' />
+				<Markdown />
+			</View>
+		</QueryClientProvider>
+	);
 }
 
+const queryClientConfig = new QueryClient();
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ components/Markdown.js
@@ -0,0 +1,16 @@
+import MarkdownDisplayer from 'react-native-markdown-display';
+import { useQuery } from 'react-query';
+import { Asset } from 'expo-asset';
+
+const getMarkdown = async () => {
+	const requiredFile = require('./../assets/test.md');
+	const asset = Asset.fromModule(requiredFile);
+	const res = await fetch(asset.uri);
+	const markdown = await res.text();
+	return markdown;
+};
+
+export default function Markdown() {
+	const { data: markdown } = useQuery(['markdown'], () => getMarkdown());
+	return <MarkdownDisplayer>{markdown}</MarkdownDisplayer>;
+}
